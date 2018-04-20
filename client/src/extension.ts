375de238572202207cc8171f5d37c4eefe4b8ca4
Ben Gummelt <camaromelt@gmail.com>
2018-04-19 19:56:07 -0700

    Initial Commit

---
+++ client/src/extension.ts
@@ -0,0 +1,123 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as path from 'path';
+
+import { workspace, ExtensionContext, WorkspaceConfiguration, Disposable } from 'vscode';
+import { 
+	LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, CancellationToken, Middleware, 
+	DidChangeConfigurationNotification, Proposed, ProposedFeatures
+} from 'vscode-languageclient';
+
+// The example settings
+interface MultiRootExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+let client: LanguageClient;
+
+namespace Configuration {
+
+	let configurationListener: Disposable;
+
+	// Convert VS Code specific settings to a format acceptable by the server. Since
+	// both client and server do use JSON the conversion is trivial. 
+	export function computeConfiguration(params: Proposed.ConfigurationParams, _token: CancellationToken, _next: Function): any[] {
+		if (!params.items) {
+			return null;
+		}
+		let result: (MultiRootExampleSettings | null)[] = [];
+		for (let item of params.items) {
+			// The server asks the client for configuration settings without a section
+			// If a section is present we return null to indicate that the configuration
+			// is not supported.
+			if (item.section) {
+				result.push(null);
+				continue;
+			}
+			let config: WorkspaceConfiguration;
+			if (item.scopeUri) {
+				config = workspace.getConfiguration('lspMultiRootSample', client.protocol2CodeConverter.asUri(item.scopeUri));
+			} else {
+				config = workspace.getConfiguration('lspMultiRootSample');
+			}
+			result.push({
+				maxNumberOfProblems: config.get('maxNumberOfProblems')
+			});
+		}
+		return result;
+	}
+	
+	export function initialize() {
+		// VS Code currently doesn't sent fine grained configuration changes. So we 
+		// listen to any change. However this will change in the near future.
+		configurationListener = workspace.onDidChangeConfiguration(() => {
+			client.sendNotification(DidChangeConfigurationNotification.type, { settings: null });
+		});
+	}
+
+	export function dispose() {
+		if (configurationListener) {
+			configurationListener.dispose();
+		}
+	}
+}
+
+
+export function activate(context: ExtensionContext) {
+
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	// The debug options for the server
+	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+	
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	}
+
+	let middleware: ProposedFeatures.ConfigurationMiddleware | Middleware = {
+		workspace: {
+			configuration: Configuration.computeConfiguration
+		}
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{scheme: 'file', language: 'javascript'},{scheme: 'file', language: 'javascriptreact'}],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contain in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+			// In the past this told the client to actively synchronize settings. Since the
+			// client now supports 'getConfiguration' requests this active synchronization is not
+			// necessary anymore. 
+			// configurationSection: [ 'lspMultiRootSample' ]
+		},
+		middleware: middleware as Middleware
+	}
+	
+	// Create the language client and start the client.
+	client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+	// Register new proposed protocol if available.
+	client.registerProposedFeatures();
+	client.onReady().then(() => {
+		Configuration.initialize();
+	});
+	
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> {
+	if (!client) {
+		return undefined;
+	}
+	Configuration.dispose();
+	return client.stop();
+}
---
+++ server/src/server.ts
@@ -0,0 +1,128 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import {
+	createConnection, TextDocuments, TextDocument, Diagnostic, DiagnosticSeverity,
+	ProposedFeatures, InitializeParams, Proposed
+} from 'vscode-languageserver';
+
+// Create a connection for the server. The connection uses Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+let connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents: TextDocuments = new TextDocuments();
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	let capabilities = params.capabilities;
+
+	// Does the client support the `workspace/configuration` request?
+	// If not, we will fall back using global settings
+	hasWorkspaceFolderCapability = (capabilities as Proposed.WorkspaceFoldersClientCapabilities).workspace && !!(capabilities as Proposed.WorkspaceFoldersClientCapabilities).workspace.workspaceFolders;
+	hasConfigurationCapability = (capabilities as Proposed.ConfigurationClientCapabilities).workspace && !!(capabilities as Proposed.ConfigurationClientCapabilities).workspace.configuration;
+
+	return {
+		capabilities: {
+			textDocumentSync: documents.syncKind
+		}
+	}
+});
+
+connection.onInitialized(() => {
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+			connection.console.log('Workspace folder change event received');
+		});
+	}
+});
+
+// The example settings
+interface MultiRootExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+const defaultSettings: MultiRootExampleSettings = { maxNumberOfProblems: 1000 };
+let globalSettings: MultiRootExampleSettings = defaultSettings;
+
+// Cache the settings of all open documents
+let documentSettings: Map<string, Thenable<MultiRootExampleSettings>> = new Map();
+
+connection.onDidChangeConfiguration(change => {
+	if (hasConfigurationCapability) {
+		// Reset all cached document settings
+		documentSettings.clear();
+	} else {
+		globalSettings = <MultiRootExampleSettings>(change.settings.lspMultiRootSample || defaultSettings);
+	}
+
+	// Revalidate all open text documents
+	documents.all().forEach(validateTextDocument);
+});
+
+function getDocumentSettings(resource: string): Thenable<MultiRootExampleSettings> {
+	if (!hasConfigurationCapability) {
+		return Promise.resolve(globalSettings);
+	}
+	let result = documentSettings.get(resource);
+	if (!result) {
+		result = connection.workspace.getConfiguration({ scopeUri: resource });
+		documentSettings.set(resource, result);
+	}
+	return result;
+}
+
+// Only keep settings for open documents
+documents.onDidClose(e => {
+	documentSettings.delete(e.document.uri);
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
+	validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+	// In this simple example we get the settings for every validate run.
+	let settings = await getDocumentSettings(textDocument.uri);
+
+	// The validator creates diagnostics for all uppercase words length 2 and more
+	let text = textDocument.getText();
+	let pattern = /\<[A-Z][a-zA-Z]+/g;
+	let m: RegExpExecArray;
+
+	let problems = 0;
+	let diagnostics: Diagnostic[] = [];
+	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
+		problems++;
+		diagnostics.push({
+			severity: DiagnosticSeverity.Warning,
+			range: {
+				start: textDocument.positionAt(m.index),
+				end: textDocument.positionAt(m.index + m[0].length)
+			},
+			message: `${m[0]} is a components`,
+			source: 'ex'
+		});
+	}
+
+	// Send the computed diagnostics to VSCode.
+	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
