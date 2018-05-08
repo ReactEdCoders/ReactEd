6e1f58fcfc475017acacc7797d60771d5a93b1b7
Ben Gummelt <camaromelt@gmail.com>
2018-05-08 08:09:40 -0700

    Removed Demo and newObj Piece

--- Demo/src/components/TotalTreats.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TotalTreats = props => <h1 id="totalTreats">{props.total}</h1>;
-
-export default TotalTreats;
-
-
--- Demo/src/components/Treat.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Treat = props => <h1 className="treat">{props.info.pic}</h1>;
-
-export default Treat;
